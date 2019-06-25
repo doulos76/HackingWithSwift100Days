3f62a0b868cd262020f927eba2d7e9749a4bbe0a
doulos76 <inojyes@gmail.com>
2019-06-26 00:00:07 +0900

    add test

--- SourceCode/Day27_CaptureListsInSwift.playground/Contents.swift
+++ SourceCode/Day27_CaptureListsInSwift.playground/Contents.swift
@@ -19,3 +19,30 @@ func sing() -> () -> Void {
 
 let singFunction = sing()
 singFunction()
+
+//: ## String capturing
+//: THis means the closure will capture any external values that are used inside the closure, and make sure they never get destoryed.
+
+//: ## Weak capturing
+
+func sing1() -> () -> Void {
+  let taylor = Singer()
+  
+  let singing = { [weak taylor] in
+    taylor?.playSong()
+    return
+  }
+  
+  return singing
+}
+
+//: ## Unowned capturing
+
+func sing2() -> () -> Void {
+  let taylor = Singer()
+  let singing = { [unowned taylor] in
+    taylor.playSong()
+    return
+  }
+  return singing
+}
